869177a134d5bf45c4f55f7519b56ac11576a49d
Nika Kvaratskhelia <nikakvaracxeliaa@gmail.com>
2023-12-16 02:32:20 +0400

    add header and footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import icon from '../assets/icons/frame.png'
+import { NavLink,Link } from 'react-router-dom'
+
+
+const Header = () => {
+  return (
+    <div>
+        <div className='fHeader'>
+            <div className="conatiner">
+                <span>"Dress for the life you deserve."</span>
+            </div>
+                <select name="select" id="select">
+                    <option value="English">English</option>
+                    <option value="Georgian">Georgian</option>
+                </select>
+        </div>
+        <header>
+            <div className="container">
+                <nav className="navbar">
+                    <Link to={"/"} className='logo'>Mariamis Atelier</Link>
+                    <ul className="lists">
+                        <NavLink className="list-item" to={"/"}>Home</NavLink>
+                        <NavLink className="list-item" to={"/products"} >Products</NavLink>
+                        <NavLink className="list-item" to={"/about"}>About</NavLink>
+                        <NavLink className="list-item" to={"/contact"}>Contact</NavLink>
+                    </ul>
+                    <img src={icon} alt="icon" />
+                </nav>
+            </div>
+        </header>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Section from '../components/section'
+
+const MainPage = () => {
+  return (
+    <div>
+        <Header/>
+        <Section/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default MainPage
