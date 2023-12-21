f6e51b3d41d0dae3630e645d3929c5e3bc56ade0
Nika Kvaratskhelia <nikakvaracxeliaa@gmail.com>
2023-12-21 18:36:03 +0400

    header and footer responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { NavLink,Link } from 'react-router-dom'
+import { FaBars } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io"
 import '../assets/styles/header.css'
 
 const Header = () => {
+    const navRef = useRef()
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav")
+    }
   return (
     <div>
         <div className='fHeader'>
@@ -12,21 +18,23 @@ const Header = () => {
         </div>
         <header>
             <div className="container">
-                <nav className="navbar">
+                <div className="header">
+                    <FaBars className='nav_btn' onClick={showNavbar}/>
                     <Link to={"/"} className='logo'>Mariamis Atelier</Link>
-                    <ul className="lists">
+                    <nav className="navbar" ref={navRef}>
                         <NavLink className="list-item" to={"/"}>Home</NavLink>
                         <NavLink className="list-item" to={"/products"} >Products</NavLink>
                         <NavLink className="list-item" to={"/about"}>About</NavLink>
                         <NavLink className="list-item" to={"/contact"}>Contact</NavLink>
-                    </ul>
+                        <IoMdClose className='nav_btn nav_close_btn' onClick={showNavbar}/>
+                    </nav>
                     <div className='languages'>
                         <select name="select" id="select">
                             <option value="English">English</option>
                             <option value="Georgian">Georgian</option>
                         </select>
                     </div>
-                </nav>
+                </div>
             </div>
         </header>
     </div>
