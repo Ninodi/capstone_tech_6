292b76fa695e69c89a08100816b44bb037268b0c
Ninodi <ninomachaidze16@gmail.com>
2024-01-05 23:24:46 +0400

    routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import './App.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, BrowserRouter, Routes, Route } from 'react-router-dom';
 import router from './router';
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import MainPage from './pages/MainPage';
+import AboutPage from './pages/AboutPage';
+import ProductPage from './pages/ProductPage';
+import ContactPage from './pages/ContactPage';
+import ProductCategoryPage from './pages/ProductCategoryPage';
+import ProductItemPage from './pages/ProductItemPage';
+// import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <BrowserRouter basename='capstone_tech_6'>
         <Routes>
-          {router.map(({ element, path }) => (
-            <Route key={path} path={path} element={element} />
-          ))}
+          <Route exact path='/capstone_tech_6' element={<MainPage/>} />
+          <Route path='/about' element={<AboutPage/>} />
+          <Route path='/products' element={<ProductPage/>} />
+          <Route path='/contact' element={<ContactPage/>} />
+          <Route path='/products/:category' element={<ProductCategoryPage/>} />
+          <Route path='/products/:category/:itemId' element={<ProductItemPage/>} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
