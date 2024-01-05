d1676c99fd7f0e777c8cbac8fc0c388e5204e044
Ninodi <ninomachaidze16@gmail.com>
2024-01-05 23:15:33 +0400

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import router from './router';
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        {router}
+        <Routes>
+          {router.map(({ element, path }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
+        </Routes>
       </Router>
     </div>
   );
