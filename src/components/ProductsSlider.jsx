83466d79916dbc5057f4099290b5301bf3d383c8
Nika Kvaratskhelia <nikakvaracxeliaa@gmail.com>
2024-01-10 23:14:21 +0400

    update product slider

--- src/components/ProductsSlider.jsx
+++ src/components/ProductsSlider.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/free-mode';
@@ -7,13 +7,31 @@ import 'swiper/css/navigation';
 import '../assets/styles/swiper.css';
 import { FreeMode, Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css/navigation';
+import useFetch from '../hooks/useFetch';
 
-const ProductsSlider = ({ slides, header }) => {
+const ProductsSlider = ({ header }) => {
+  const {response} = useFetch({url: `http://94.137.187.198:9876/products/`, method: 'GET'})
+  const [slidesPerView, setSlidesPerView] = useState(3);
+
+  useEffect(() => {
+    const updateSlidesPerView = () => {
+      const newSlidesPerView = window.innerWidth <= 450 ? 1 : 3;
+      setSlidesPerView(newSlidesPerView);
+    };
+    updateSlidesPerView();
+    window.addEventListener('resize', updateSlidesPerView);
+
+    return () => {
+      window.removeEventListener('resize', updateSlidesPerView);
+    };
+  }, []);
   return (
+    
     <div className="container">
+      
       <h2 className='heading'>{header}</h2>
       <Swiper
-        slidesPerView={3}
+        slidesPerView={slidesPerView}
         spaceBetween={143}
         freeMode={true}
         pagination={{
@@ -23,10 +41,10 @@ const ProductsSlider = ({ slides, header }) => {
         modules={[FreeMode, Pagination, Navigation]}
         className="mySwiper"
       >
-        {slides.map((slide, index) => (
-          <SwiperSlide key={index}>
-            <img src={slide.img} alt={slide.alt} />
-            <h4>{slide.heading}</h4>
+        {response?.map((product) => (
+          <SwiperSlide key={product.id}>
+            <img className='slider-image' src={product.image} alt={product.product_name} />
+            <h4>{product.product_name}</h4>
           </SwiperSlide>
         ))}
       </Swiper>
