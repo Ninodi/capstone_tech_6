980b0f0a94cfc74890c959a3302b773fb7e7384d
Nika Kvaratskhelia <nikakvaracxeliaa@gmail.com>
2024-01-18 01:26:36 +0400

    add useFetch on header and productpage

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,39 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import ProductsSlider from '../components/ProductsSlider'
 import BestSeller from '../components/BestSeller'
 import { useTranslation } from 'react-i18next'
+import useFetch from '../hooks/useFetch'
 const ProductPage = () => {
   const { t } = useTranslation();
-  const [products, setProducts] = useState([]);
 
+
+  const { response: products, onFetch } = useFetch({
+    url: 'http://94.137.187.198:9876/products/',
+    method: 'GET',
+  });
 
   useEffect(() => {
-    fetch(`http://94.137.187.198:9876/products/`, {
-      method: 'GET',
-      headers: {
-        "content-type": "application/json",
-      }
-    })
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error("Failed to get response");
-      }
-      return res.json();
-    })
-    .then((data) =>{
-      setProducts(data);
-    })
-    
-  },[])
+    onFetch();
+  }, [onFetch]);
 
-  const categoryFourProducts = products.filter((product) => product.category === 1 );
-  const categoryTwoProducts = products.filter((product) => product.category === 4);
+  const categoryFourProducts = products && products.filter((product) => product.category === 1 );
+  const categoryTwoProducts = products && products.filter((product) => product.category === 4);
   return (
     <div>
         <Header/>
-        <ProductsSlider products={categoryFourProducts}  header={t('productPage.firstHeadline')} />
+        <ProductsSlider products={categoryFourProducts}  header={t('productPage.firstHeadline')}/>
         <BestSeller />
         <ProductsSlider  products={categoryTwoProducts} header={t('productPage.secondHeadline')}/>
         <Footer/>
