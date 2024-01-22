d4119fa5e9f753bd77b392aa041365e55b7aa33d
Ninodi <ninomachaidze16@gmail.com>
2024-01-22 17:38:25 +0400

    add grid toggle to products in mobile

--- src/components/ProductDisplaySettings.jsx
+++ src/components/ProductDisplaySettings.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import SingleCol from '../assets/img/singleColumnDisplay.png'
 import TwoCol from '../assets/img/twoColumnDisplay.png'
 
-function ProductDisplaySettings({filteredProd}) {
+function ProductDisplaySettings({filteredProd, setToggleColumns}) {
   return (
     <div className='prod-display-sett'>
         <p>Products</p>
         <div className="display-settings">
             <p>{filteredProd?.length} items</p>
             <div className="display-options">
-                <div className="single-column" style={{width: '20px', height: '20px'}}>
+                <div className="single-column" style={{width: '20px', height: '20px'}} onClick={prev => setToggleColumns(true)}> 
                     <img src={SingleCol} alt="" style={{width: '100%', height: '100%'}}/>
                 </div>
-                <div className="two-columns" style={{width: '20px', height: '20px'}}>
+                <div className="two-columns" style={{width: '20px', height: '20px'}} onClick={prev => setToggleColumns(false)}>
                     <img src={TwoCol} alt="" style={{width: '100%', height: '100%'}}/>
                 </div>
             </div>
