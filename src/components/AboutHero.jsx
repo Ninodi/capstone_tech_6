24826e18a57c0a42285b1b01fc19b871679b88ad
Nika Kvaratskhelia <nikakvaracxeliaa@gmail.com>
2024-01-23 10:17:39 +0400

    .

--- src/components/AboutHero.jsx
+++ src/components/AboutHero.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Slide1 from '../assets/img/slide1.png'
 import '../assets/styles/AboutPage.css'
+import { useTranslation } from 'react-i18next'
 
 function AboutHero() {
+  const {t} = useTranslation()
   return (
     <div className='about-hero'>
         <img src={Slide1} alt="" />
         <div className="about-hero-title">
-            <h1>About us</h1>
-            <p>Home/About Us</p>
+            <h1>{t("aboutUs.aboutUs")}</h1>
+            <p>{t("aboutUs.home")} {t("aboutUs.aboutUs")} </p>
         </div>
     </div>
   )
